7e1d0602d4e8e21ce01e59ca08c9d91beadbd8cd
TimotheeRoy <timroy93100@gmail.com>
2024-04-01 01:17:20 +0200

    cors disable

--- api/index.js
+++ api/index.js
@@ -5,8 +5,7 @@ const app = express();
 const cors = require("cors");
 
 const corsOptions = {
-    origin: ["https://le-comptoir-hnq0r702q-tiimooys-projects.vercel.app/"],
-    optionSuccesStatus: 200,
+    origin: false
 };
 
 app.use(cors(corsOptions));
